205584ec02fac19f20aaafeccf98373b11ced347
JaimeCodeSpace <jaimeramos7777@gmail.com>
2025-08-02 22:20:17 -0600

    Se ha terminado el challenge

--- app.js
+++ app.js
@@ -1,1 +1,39 @@
 // El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el problema.
+let amigos = [];
+
+function agregarAmigo(){
+
+    let nombre = document.getElementById('amigo').value;
+    if(nombre == ''){
+        alert("Agregue un nombre, ¡por favor!")
+    } else{
+        //Validar que el nombre ya existe
+        if(amigos.includes(nombre)){
+            alert("El nombre ya esta en la lista");
+        }
+        amigos.push(nombre);
+        document.querySelector('#amigo').value = '';
+        asignarElemento('listaAmigos', nombre);
+        
+    }
+}
+
+
+
+ 
+function sortearAmigo(){
+    if(amigos == ''){
+        alert("No hay amigos por sortear");
+    } else{
+
+    let amigoSorteado = Math.floor( Math.random() * amigos.length);
+    asignarElemento('resultado', "El amigo secreto es: " + amigos[amigoSorteado]);
+    console.log(amigoSorteado);
+    }
+    asignarElemento('listaAmigos', '');
+}
+
+function asignarElemento(elemento, texto){
+    let elementoHTML = document.getElementById(elemento);
+    elementoHTML.innerHTML = texto;
+}
